e0f5313c4c1b5fdd6d095dfe94323300682416f9
tauchner <7229@htl.rennweg.at>
2021-04-07 11:20:54 +0200

    Added Template Test

---
+++ website/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+require 'nav.php';
+?>
+<h1>THIS IS A TEMPLATE :DD</h1>
+<?php
+require 'footer.php';
+?>
+</body>
+</html>
