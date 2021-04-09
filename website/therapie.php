3bb6848d0a879f2c6ff1e6c9c45367b150fa2ea0
tauchner <7229@htl.rennweg.at>
2021-04-10 00:28:43 +0200

    Made the code readable

--- website/therapie.php
+++ website/therapie.php
@@ -7,93 +7,129 @@
     <meta charset="UTF-8">
     <title>F.A.S.T</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
-          rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
           rel="stylesheet">
     <script src="therapie.js" defer></script>
-    <link rel="stylesheet" href="therapie.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <div id="heading">
-    <p id="headline">Therapie</p>
+    <h1>Therapie</h1>
 </div>
 <section id="content">
-    <div id="first">
+    <div class="contentSection">
         <div class="contentHeading">
-            klassische Massage
+            <h2>klassische Massage</h2>
         </div>
         <div class="description">
-            <img class="plus" src="../images/plus.png">
-            <p>löst Verspannungen und Verkrampfungen in der Muskulatur</p>
-            <img class="plus" src="../images/plus.png">
-            <p>wohltuend, durchblutungsfördernd</p>
-            <img class="plus" src="../images/plus.png">
-            <p>löst Verspannungen und Verkrampfungen in der Muskulatur</p>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>löst Verspannungen und Verkrampfungen in der Muskulatur</a>
+            </div>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>wohltuend, durchblutungsfördernd</a>
+            </div>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>löst Verspannungen und Verkrampfungen in der Muskulatur</a>
+            </div>
         </div>
     </div>
 
-    <div id="second">
+    <div class="contentSection">
         <div class="contentHeading">
-            Sportmassage
+            <h2>Sportmassage</h2>
         </div>
         <div class="description">
-            <img class="plus" src="../images/plus.png">
-            <p>regenerative Massage nach wettkampfmäßiger Belastung</p>
-            <img class="plus" src="../images/plus.png">
-            <p>Behandlung von sportartspezifischen Verletzungen und Restriktionen</p>
-            <img class="plus" src="../images/plus.png">
-            <p>Prävention von Verletzungen durch rechtzeitiges behandeln von Verhärtungen und Verspannung</p>
-            <img class="plus" src="../images/plus.png">
-            <p>Analyse und Behandlung von strukturellen Problematiken</p>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>regenerative Massage nach wettkampfmäßiger Belastung</a>
+            </div>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>Behandlung von sportartspezifischen Verletzungen und Restriktionen</a>
+            </div>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>Prävention von Verletzungen durch rechtzeitiges behandeln von Verhärtungen und Verspannung</a>
+            </div>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>Analyse und Behandlung von strukturellen Problematiken</a>
+            </div>
         </div>
     </div>
 
-    <div id="third">
+    <div class="contentSection">
         <div class="contentHeading">
-            Sportmassage
+            <h2>Sportmassage</h2>
         </div>
         <div class="description">
-            <img class="plus" src="../images/plus.png">
-            <p>regenerative Massage nach wettkampfmäßiger Belastung</p>
-            <img class="plus" src="../images/plus.png">
-            <p>Behandlung von sportartspezifischen Verletzungen und Restriktionen</p>
-            <img class="plus" src="../images/plus.png">
-            <p>Prävention von Verletzungen durch rechtzeitiges behandeln von Verhärtungen und Verspannung</p>
-            <img class="plus" src="../images/plus.png">
-            <p>Analyse und Behandlung von strukturellen Problematiken</p>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>regenerative Massage nach wettkampfmäßiger Belastung</a>
+            </div>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>Behandlung von sportartspezifischen Verletzungen und Restriktionen</a>
+            </div>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>Prävention von Verletzungen durch rechtzeitiges behandeln von Verhärtungen und Verspannung</a>
+            </div>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>Analyse und Behandlung von strukturellen Problematiken</a>
+            </div>
         </div>
     </div>
 
-    <div id="fourth">
+    <div class="contentSection">
         <div class="contentHeading">
-            Sportmassage
+            <h2>Sportmassage</h2>
         </div>
         <div class="description">
-            <img class="plus" src="../images/plus.png">
-            <p>regenerative Massage nach wettkampfmäßiger Belastung</p>
-            <img class="plus" src="../images/plus.png">
-            <p>Behandlung von sportartspezifischen Verletzungen und Restriktionen</p>
-            <img class="plus" src="../images/plus.png">
-            <p>Prävention von Verletzungen durch rechtzeitiges behandeln von Verhärtungen und Verspannung</p>
-            <img class="plus" src="../images/plus.png">
-            <p>Analyse und Behandlung von strukturellen Problematiken</p>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>regenerative Massage nach wettkampfmäßiger Belastung</a>
+            </div>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>Behandlung von sportartspezifischen Verletzungen und Restriktionen</a>
+            </div>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>Prävention von Verletzungen durch rechtzeitiges behandeln von Verhärtungen und Verspannung</a>
+            </div>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>Analyse und Behandlung von strukturellen Problematiken</a>
+            </div>
         </div>
     </div>
 
-    <div id="fifth">
+    <div class="contentSection">
         <div class="contentHeading">
-            Sportmassage
+            <h2>Sportmassage</h2>
         </div>
         <div class="description">
-            <img class="plus" src="../images/plus.png">
-            <p>regenerative Massage nach wettkampfmäßiger Belastung</p>
-            <img class="plus" src="../images/plus.png">
-            <p>Behandlung von sportartspezifischen Verletzungen und Restriktionen</p>
-            <img class="plus" src="../images/plus.png">
-            <p>Prävention von Verletzungen durch rechtzeitiges behandeln von Verhärtungen und Verspannung</p>
-            <img class="plus" src="../images/plus.png">
-            <p>Analyse und Behandlung von strukturellen Problematiken</p>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>regenerative Massage nach wettkampfmäßiger Belastung</a>
+            </div>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>Behandlung von sportartspezifischen Verletzungen und Restriktionen</a>
+            </div>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>Prävention von Verletzungen durch rechtzeitiges behandeln von Verhärtungen und Verspannung</a>
+            </div>
+            <div class="item">
+                <img class="plus" src="images/plus.svg" alt="plus icon">
+                <a>Analyse und Behandlung von strukturellen Problematiken</a>
+            </div>
         </div>
     </div>
 </section>
