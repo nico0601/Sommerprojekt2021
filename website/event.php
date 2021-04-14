a1abc10ca3512353b7a1c32a345e1a58396cdb63
Christoph Samuel <chsamuel@gmx.at>
2021-04-14 14:51:37 +0200

    added first design of event.php with event.scss

---
+++ website/event.php
@@ -0,0 +1,48 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>F.A.S.T - Events</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="event.css">
+</head>
+<body>
+<div id="heading">
+    <h1>Events</h1>
+</div>
+<section id="content">
+    <div class="contentSection">
+        <div class="description">
+            <div class="item">
+                <img class="event" src="images/Event.jpg" alt="Event">
+            </div>
+            <div class="item">
+                <img class="event" src="images/Event.jpg" alt="Event">
+            </div>
+            <div class="item">
+                <img class="event" src="images/Event.jpg" alt="Event">
+            </div>
+            <div class="item">
+                <img class="event" src="images/Event.jpg" alt="Event">
+            </div>
+            <div class="item">
+                <img class="event" src="images/Event.jpg" alt="Event">
+            </div>
+        </div>
+        <div class="contentInfo">
+            <p>Bei Interesse entweder</p>
+            <p class="bold">telefonisch (<span class="blue">+43 660 12345678</span>)</p>
+            <p>oder</p>
+            <p class="bold">per Email (<span class="blue">werdenichc@hotmail.com</span>)</p>
+            <p>Kontakt aufnehmen</p>
+        </div>
+    </div>
+</section>
+</body>
+</html>
