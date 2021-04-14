abe22997018945e891e844caa066717bcfe3b3c8
Christoph Samuel <chsamuel@gmx.at>
2021-04-14 18:34:17 +0200

    added first design of kontakt.php with kontakt.scss

---
+++ website/kontakt.php
@@ -0,0 +1,51 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>F.A.S.T - Kontakt</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="kontakt.css">
+</head>
+<body>
+<div id="heading">
+    <h1>Kontakt</h1>
+</div>
+<section id="content">
+    <div class="contentSection">
+        <div class="description">
+            <div class="item">
+                <p>Christian Werdenich</p>
+                <p>Telefon:</p>
+                <p class="right"><span class="blue">+43</span> 660 12345678</p>
+                <p>Email:</p>
+                <p class="right">werdenichc<span class="blue">@</span>hotmail.com</p>
+                <div id="socialMediaDiv">
+                    <a href="https://www.instagram.com/fascial_sportstherapy/">
+                        <img class="socialMedia" src="images/Instagram.png" alt="Instagram">
+                    </a>
+                    <a href="https://www.instagram.com/fascial_sportstherapy/">
+                        <img class="socialMedia" src="images/Facebook.png" alt="Facebook">
+                    </a>
+                </div>
+            </div>
+            <div class="item">
+                <p>Impressum</p>
+                <p>Medieninhaber</p>
+                <div>
+                    <p class="right">Christian Werdenich</p>
+                    <p class="right">Das Haus am Toast <span class="blue">22</span></p>
+                    <p class="right"><span class="blue">1230</span> Wieeeha</p>
+                </div>
+                <p class="right2">werdenichc<span class="blue">@</span>hotmail.com</p>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
