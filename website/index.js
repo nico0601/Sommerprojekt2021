61b1300de87a24a7158fc61d77c8dd2466b931a5
Christoph Samuel <chsamuel@gmx.at>
2021-04-20 20:31:50 +0200

    added first design of landingpage.php with landingpage.scss

---
+++ website/index.js
@@ -0,0 +1,90 @@
+let slideIndex1 = 1
+showDivs1(slideIndex1)
+
+let slideIndex2 = 1
+showDivs2(slideIndex2)
+
+let left1 = document.getElementById("buttonLeft1")
+let right1 = document.getElementById("buttonRight1")
+
+let left2 = document.getElementById("buttonLeft2")
+let right2 = document.getElementById("buttonRight2")
+
+left1.addEventListener('click', minusDivs1, false)
+right1.addEventListener('click', plusDivs1, false)
+
+left2.addEventListener('click', minusDivs2, false)
+right2.addEventListener('click', plusDivs2, false)
+
+
+
+function plusDivs1() {
+    let x = document.querySelectorAll(".therapie")
+    if (!(slideIndex1 + 1 > x.length)) {
+        showDivs1(slideIndex1 += 1)
+        left1.style.opacity = 1
+        left1.style.cursor = 'pointer'
+    }
+    if (slideIndex1 + 1 > x.length) {
+        right1.style.opacity = 0.5
+        right1.style.cursor = 'auto'
+    }
+}
+
+function plusDivs2() {
+    let x = document.querySelectorAll(".training")
+    if (!(slideIndex2 + 1 > x.length)) {
+        showDivs2(slideIndex2 += 1)
+        left2.style.opacity = 1
+        left2.style.cursor = 'pointer'
+    }
+    if (slideIndex2 + 1 > x.length) {
+        right2.style.opacity = 0.5
+        right2.style.cursor = 'auto'
+    }
+}
+
+
+function minusDivs1() {
+    if (!(slideIndex1 - 1 <= 0)) {
+        showDivs1(slideIndex1 -= 1)
+        right1.style.opacity = 1
+        right1.style.cursor = 'pointer'
+    }
+    if (slideIndex1 - 1 <= 0) {
+        left1.style.opacity = 0.5
+        left1.style.cursor = 'auto'
+    }
+}
+
+function minusDivs2() {
+    if (!(slideIndex2 - 1 <= 0)) {
+        showDivs2(slideIndex2 -= 1)
+        right2.style.opacity = 1
+        right2.style.cursor = 'pointer'
+    }
+    if (slideIndex2 - 1 <= 0) {
+        left2.style.opacity = 0.5
+        left2.style.cursor = 'auto'
+    }
+}
+
+
+
+function showDivs1(n) {
+    let x = document.querySelectorAll(".therapie")
+
+    for (let i = 0; i < x.length; i++) {
+        x[i].style.display = 'none'
+    }
+    x[slideIndex1 - 1].style.display = 'block'
+}
+
+function showDivs2(n) {
+    let x = document.querySelectorAll(".training")
+
+    for (let i = 0; i < x.length; i++) {
+        x[i].style.display = 'none'
+    }
+    x[slideIndex2 - 1].style.display = 'block'
+}
---
+++ website/index.php
@@ -0,0 +1,98 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>F.A.S.T</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet">
+    <script src="index.js" defer></script>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+<div id="video">
+    <img src="images/laufen.jpg" alt="Video">
+    <div>
+        <img id="logo" src="images/logo%20-%20hell.png" alt="Logo">
+        <div id="buttonDiv">
+            <a href="kontakt.php">
+                <button type="button" class="button infoButton">Kontakt</button>
+            </a>
+            <a href="termin.php">
+                <button type="button" class="button infoButton">Termin</button>
+            </a>
+        </div>
+        <p id="fastText">Fascial Applied Sports Therapy</p>
+    </div>
+</div>
+<div id="heading">
+    <h1>Angebote</h1>
+</div>
+<section id="content">
+    <div class="contentSection">
+        <div class="description">
+            <img src="images/pfeil.png" class="nav" id="buttonLeft1">
+            <div class="item therapie">
+                <a href="therapie.php">
+                    <button type="button" class="button">klassische Massage</button>
+                </a>
+            </div>
+            <div class="item therapie">
+                <a href="therapie.php">
+                    <button type="button" class="button">Sportmassage</button>
+                </a>
+            </div>
+            <div class="item therapie">
+                <a href="therapie.php">
+                    <button type="button" class="button">Faszientherapie</button>
+                </a>
+            </div>
+            <div class="item therapie">
+                <a href="therapie.php">
+                    <button type="button" class="button">FUZO</button>
+                </a>
+            </div>
+            <div class="item therapie">
+                <a href="therapie.php">
+                    <button type="button" class="button">Lymphdrainage</button>
+                </a>
+            </div>
+            <img src="images/pfeil.png" class="nav" id="buttonRight1">
+        </div>
+        <div class="description">
+            <img src="images/pfeil.png" class="nav" id="buttonLeft2">
+            <div class="item training">
+                <a href="training.php">
+                    <button type="button" class="button">Functional Training</button>
+                </a>
+            </div>
+            <div class="item training">
+                <a href="training.php">
+                    <button type="button" class="button">Faszientraining</button>
+                </a>
+            </div>
+            <div class="item training">
+                <a href="training.php">
+                    <button type="button" class="button">Speedtraining</button>
+                </a>
+            </div>
+            <div class="item training">
+                <a href="training.php">
+                    <button type="button" class="button">Testing</button>
+                </a>
+            </div>
+            <div class="item training">
+                <a href="training.php">
+                    <button type="button" class="button">Nutrition, Gewichtsmanagement</button>
+                </a>
+            </div>
+            <img src="images/pfeil.png" class="nav" id="buttonRight2">
+        </div>
+    </div>
+</section>
+</body>
+</html>
