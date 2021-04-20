763ff3fb329b45b2b38b7f25e98554a5ec2a9099
tauchner <7229@htl.rennweg.at>
2021-04-20 14:05:38 +0200

    Website is in a state where it can be tested

--- website/index.php
+++ website/index.php
@@ -4,13 +4,19 @@
   <?php
   include "header.php"
   ?>
-  <title>F.A.S.T - Events</title>
-  <link rel="stylesheet" href="event.css">
+    <title>F.A.S.T - Home</title>
 </head>
 <body>
 <?php
 include "nav.php"
 ?>
+
+<div id="heading">
+    <h1>Oh nein!</h1>
+</div>
+
+<p>Bis jetzt hat sich leider noch niemand erbarmt, diese Website zu machen. Ich muss jetzt leider aber anfangen,
+    die Seite zu testen, und da will ich eine Homepage haben.... Drum hier eine Homepage :DD</p>
 
 <?php
 include "footer.php"
