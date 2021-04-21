427de8cf855f0f85361df8faf59501073b10149c
tauchner <7229@htl.rennweg.at>
2021-04-21 09:13:51 +0200

    Added heaer and footer to all pages

--- website/index.php
+++ website/index.php
@@ -4,16 +4,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+  <?php
+  include "header.php";
+  ?>
     <title>F.A.S.T</title>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-          rel="stylesheet">
-    <script src="index.js" defer></script>
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
+<?php
+include "nav.php";
+?>
 <div id="video">
     <img src="images/laufen.jpg" alt="Video">
     <div>
@@ -95,5 +95,8 @@
     </div>
 </section>
 <img src="images/weiter.png" alt="weiter" id="weiter">
+<?php
+include "footer.php";
+?>
 </body>
 </html>
--- website/termin.php
+++ website/termin.php
@@ -4,15 +4,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+  <?php
+  include "header.php";
+  ?>
     <title>F.A.S.T - Termine</title>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-          rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="termin.css">
 </head>
 <body>
+<?php
+include "nav.php";
+?>
 <div id="heading">
     <h1>Termine</h1>
 </div>
@@ -85,5 +86,8 @@
         </div>
     </div>
 </section>
+<?php
+include "footer.php";
+?>
 </body>
 </html>
