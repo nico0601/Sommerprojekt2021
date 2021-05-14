e0bd2a65674546aed0f736937ff4f53d2853d38b
Christoph Samuel <chsamuel@gmx.at>
2021-05-14 16:50:09 +0200

    fixed scrollIntoView when Therapie or Training is clicked (centered)

--- website/therapieTraining.js
+++ website/therapieTraining.js
@@ -3,14 +3,16 @@ let content = document.querySelectorAll('.contentHeading');
 content.forEach(element => element.addEventListener('click', expandContent, false));
 
 function expandContent(event) {
-    let contentDetails = event.target.parentNode.querySelector('.description');
+    let contentDetails = event.target.parentNode.querySelector('.description')
+    let scroll = event.target.parentNode
 
     if (contentDetails.classList.contains('hidden')) {
         contentDetails.classList.remove('hidden');
-        contentDetails.scrollIntoView({behavior: "smooth", block: "start"})
+        scroll.scrollIntoView({behavior: "smooth", block: "center"})
     } else if (contentDetails.classList.contains('noHidden')) {
         contentDetails.classList.remove('noHidden');
-        contentDetails.scrollIntoView({behavior: "smooth", block: "start"})
+        contentDetails.classList.add('hidden');
+        scroll.scrollIntoView({behavior: "smooth", block: "center"})
     } else {
         contentDetails.classList.add('hidden');
     }
