124da92203871cd6a9abdfa3a33a4bea6975dd34
Christoph Samuel <chsamuel@gmx.at>
2021-05-14 22:52:51 +0200

    changed "ueberMich.php" content-source from static to database

--- website/ueberMich.php
+++ website/ueberMich.php
@@ -31,17 +31,17 @@ include "nav.php"
             </div>
             <div class="item">
                 <p class="blue oswald left3">Information</p>
-                <p id="descriptionText">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Quisque fringilla ultrices neque et fringilla. Aliquam euismod enim ante,
-                    in aliquet dui porta nec. Integer ut placerat metus. Ut pulvinar mauris quis
-                    risus auctor venenatis. Aenean at lobortis turpis. Cras suscipit viverra euismod.
-                    Nunc aliquet ipsum non magna gravida porta. Mauris aliquam enim velit, ac dignissim
-                    nisi efficitur at. Curabitur vitae diam quis odio sagittis tincidunt ut ut velit.
-                    Nam leo ligula, vulputate quis scelerisque eu, posuere viverra risus.
-                    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                <?php
+                include "getPDO.php";
 
-                    Vivamus magna erat, rhoncus vel elementum eu, venenatis at ligula.
-                    Morbi dictum mi ac orci scelerisque mattis.</p>
+                $stmt = getPDO()->prepare("SELECT * FROM ueber_mich");
+                $stmt->execute();
+                $stmt = $stmt->fetchAll();
+
+                foreach ($stmt as $ueber_mich) {
+                    echo "<p id='descriptionText'>$ueber_mich[1]</p>";
+                }
+                ?>
             </div>
         </div>
     </div>
