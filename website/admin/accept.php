817d309c54278b4a2516d78a48c3a8bdc18631d0
Christoph Samuel <chsamuel@gmx.at>
2021-05-29 00:12:53 +0200

    set max-file size for new Event to 50000000 Bytes

--- website/admin/accept.php
+++ website/admin/accept.php
@@ -3,13 +3,15 @@ session_start();
 include_once "getPDO.php";
 require_once "Event.php";
 
-if (isset($_FILES['event']['tmp_name']) && $_FILES['event']['tmp_name'] != "") {
+if (isset($_FILES['event']['tmp_name']) && $_FILES['event']['tmp_name'] != "" &&
+    filesize($_FILES['event']['tmp_name']) < 50000000) {
+
     $saveFolder = '../images/';
     copy($_FILES['event']['tmp_name'], $saveFolder . $_FILES['event']['name']);
 
     $saveFolder = '/images/';
-    echo "Save in: ".$saveFolder . $_FILES['event']['name'];
     $_SESSION['file'] = $saveFolder . $_FILES['event']['name'];
+    $_SESSION['size'] = filesize($_FILES['event']['tmp_name']);
 }
 
 header("Location: editEvent.php");
