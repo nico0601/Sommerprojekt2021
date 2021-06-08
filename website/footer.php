84115a21e46e72a58eea131a40734b2fb6882857
Christoph Samuel <chsamuel@gmx.at>
2021-06-08 23:13:23 +0200

    added "Christian Werdenich" to footer

--- website/footer.php
+++ website/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <a href="/kontakt">Kontakt</a> | <a href="/kontakt">Impressum</a> | <a href="/admin">Admin Bereich</a>
+    <a href="/kontakt">Kontakt</a> | <a href="/agb.php">Impressum</a> | <a href="/admin">Admin Bereich</a>
     <br><br>
-    &copy; 2021 <a href="https://github.com/christoph-samuel">Christoph Samuel</a>
+    &copy; 2021 Christian Werdenich
 </footer>
