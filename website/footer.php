1ba2f07bc9e3d005cb83631c9c02a6404382e811
tauchner <7229@htl.rennweg.at>
2021-06-08 22:57:17 +0200

    removed myself from the footer

--- website/footer.php
+++ website/footer.php
@@ -1,6 +1,5 @@
 <footer>
     <a href="/kontakt">Kontakt</a> | <a href="/kontakt">Impressum</a> | <a href="/admin">Admin Bereich</a>
     <br><br>
-    &copy; 2021 <a href="https://github.com/Tiefseetauchner">Christoph Tauchner</a>,
-    <a href="https://github.com/christoph-samuel">Christoph Samuel</a>
+    &copy; 2021 <a href="https://github.com/christoph-samuel">Christoph Samuel</a>
 </footer>
